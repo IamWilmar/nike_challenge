0933bf72db1597ba30ef103eb131a5d0390098b8
IamWilmar <wilmardaniel25@gmail.com>
2021-02-27 22:58:57 -0500

    Animation of Searcgbar done

--- lib/pages/ecommerce_pages/main_store_page.dart
+++ lib/pages/ecommerce_pages/main_store_page.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
 
-class MainStorePage extends StatelessWidget {
+class MainStorePage extends StatefulWidget {
+  @override
+  _MainStorePageState createState() => _MainStorePageState();
+}
+
+class _MainStorePageState extends State<MainStorePage> {
+  ScrollController scrollController = ScrollController();
+  final ValueNotifier<bool> searchBarVisible = ValueNotifier(true);
+  double scrollAnterior = 0.0;
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      print(scrollController.offset);
+      if (scrollController.offset < scrollAnterior - 10) {
+        searchBarVisible.value = true;
+      } else if (scrollController.offset > scrollAnterior + 10) {
+        searchBarVisible.value = false;
+      }
+      if(scrollController.offset < 0.0){
+        searchBarVisible.value = true;
+      }
+      if(scrollController.offset > scrollController.position.maxScrollExtent){
+        searchBarVisible.value = false;
+      }
+      scrollAnterior = scrollController.offset;
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context). size;
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
@@ -13,6 +48,7 @@ class MainStorePage extends StatelessWidget {
               children: [
                 Expanded(
                   child: ListView(
+                    controller: scrollController,
                     padding: EdgeInsets.only(bottom: 50, top: 220),
                     physics: BouncingScrollPhysics(),
                     children: [
@@ -31,16 +67,27 @@ class MainStorePage extends StatelessWidget {
               ],
             ),
           ),
-          Positioned(
-            left: 0,
-            right: 0,
-            height: 220,
+          ValueListenableBuilder<bool>(
+            valueListenable: searchBarVisible,
             child: Container(
               decoration: BoxDecoration(
                 color: Color(0xFF1A1924),
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(35), bottomRight: Radius.circular(35)),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(35),
+                    bottomRight: Radius.circular(35)),
               ),
             ),
+              builder: (context, value, child){
+                return AnimatedPositioned(
+                  curve: Curves.easeIn,
+                  duration: Duration(seconds: 6),
+                  left: 0,
+                  right: 0,
+                  top: value ? 0 : -220,
+                  height: 220,
+                  child: child,
+                );
+              },
           ),
         ],
       ),
